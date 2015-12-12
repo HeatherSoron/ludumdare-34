9320484e514977ff2f073e7dfe15a16599585751
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2015-12-13 12:59:23 +1300

    Move terrain gen code into a separate file

--- js/main.js
+++ js/main.js
@@ -18,63 +18,6 @@ var seaWidth = seaTiles * tileSize;
 
 var lines = [];
 
-function makeTerrain() {
-	var seaLevel = game.height - (game.height / 4);
-
-	// array of segment heights
-	var segments = [0, 1, 0];
-
-	for (var i = 0; i < terrainIterations; ++i) {
-		// increment by 2 each time, because we add another element each iteration
-		// also, skip the last iteration, because otherwise j+1 is undefined
-		for (var j = 0; j < segments.length - 1; j += 2) {
-			var l = segments[j];
-			var r = segments[j + 1];
-			// math black magic: take a random percentage (0% to 100%) of the difference in height, and then add that on top of the initial height
-			var mid = ((r - l) * Math.random()) + l;
-			// insert into the middle of the array
-			segments.splice(j + 1, 0, mid);
-		}
-	}
-
-
-	var width = islandWidth / (segments.length - 1);
-
-	segments = segments.map(function(elev) {
-		var height = (1 - elev) * seaLevel;
-		// round it to the step size of our tiles
-		height = Math.round(height / stepSize) * stepSize;
-		return height;
-	});
-
-	for (var i = 0; i < segments.length - 1; ++i) {
-		var x = i * width;
-		var left = segments[i];
-		var right = segments[i + 1];
-		lines.push(new Phaser.Line(x, left, x + width, right));
-		
-		// do left - right because negative numbers are UP
-		var steps = (left - right) / stepSize;
-		var flip = false;
-		if (steps < 0) {
-			console.log(left, right, steps, 4 - (-steps));
-			flip = true;
-			steps = -steps;
-			// compensate for the flip
-			x += tileSize;
-		}
-		var frame = 4 - steps;
-		if (frame == 4) {
-			frame = 7;
-		}
-		var tile = islandGroup.create(x, Math.min(left, right), 'terrain');
-		tile.body.immovable = true;
-		tile.frame = frame;
-		if (flip) {
-			tile.scale.x = -1;
-		}
-	}
-}
 
 function preload() {
 	game.load.image('star', 'assets/star.png');
