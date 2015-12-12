195f8daf218bdc9fc4a3d249f4ad851efb742a54
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2015-12-12 17:23:02 +1300

    Add gravity between the player and the big thing

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var player;
 var cursors;
+var heavy;
 
 function preload() {
 	game.load.image('star', 'assets/star.png');
@@ -11,7 +12,7 @@ function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
 	player = game.add.sprite(0, 400, 'star');
-	game.add.sprite(200, 200, 'diamond');
+	heavy = game.add.sprite(200, 200, 'diamond');
 
 	game.physics.arcade.enable(player);
 	player.body.velocity.x = 60;
@@ -21,9 +22,20 @@ function create() {
 
 function update() {
 	var rotSpeed = 0.05;
+	var gravPower = 3;
+
 	if (cursors.left.isDown) {
 		player.body.velocity.rotate(0, 0, -rotSpeed);
 	} else if (cursors.right.isDown) {
 		player.body.velocity.rotate(0, 0, rotSpeed);
 	}
+
+	var offset = heavy.position.clone().subtract(player.body.position.x, player.body.position.y);
+	var gravDir = offset.normalize();
+
+	var distSq = offset.getMagnitudeSq();
+
+	var grav = gravDir.setMagnitude(gravPower / distSq);
+
+	player.body.velocity = player.body.velocity.add(grav.x, grav.y);
 }
