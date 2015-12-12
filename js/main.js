a72d1d49b96742983e3d8d90baef5d2b593f45b8
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2015-12-12 19:13:39 +1300

    YAY TERRAIN GEN! Makes pretty cliffs now :)

--- js/main.js
+++ js/main.js
@@ -11,7 +11,23 @@ function makeTerrain() {
 	var iterations = 10;
 	var seaLevel = game.height - (game.height / 4);
 
+	// array of segment heights
 	var segments = [0, 1, 0];
+
+	for (var i = 0; i < iterations; ++i) {
+		// increment by 2 each time, because we add another element each iteration
+		// also, skip the last iteration, because otherwise j+1 is undefined
+		for (var j = 0; j < segments.length - 1; j += 2) {
+			var l = segments[j];
+			var r = segments[j + 1];
+			// math black magic: take a random percentage (0% to 100%) of the difference in height, and then add that on top of the initial height
+			var mid = ((r - l) * Math.random()) + l;
+			// insert into the middle of the array
+			segments.splice(j + 1, 0, mid);
+		}
+	}
+
+	console.log(segments);
 
 
 	var width = game.width / (segments.length - 1);
