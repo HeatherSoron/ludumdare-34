a891169cb0a0d7f109092538b994e5890dc9db93
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2015-12-12 17:01:03 +1300

    Make the star (player) move and be steerable

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var player;
-
+var cursors;
 
 function preload() {
 	game.load.image('star', 'assets/star.png');
@@ -15,8 +15,15 @@ function create() {
 
 	game.physics.arcade.enable(player);
 	player.body.velocity.x = 60;
+
+	cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
-		
+	var rotSpeed = 0.05;
+	if (cursors.left.isDown) {
+		player.body.velocity.rotate(0, 0, -rotSpeed);
+	} else if (cursors.right.isDown) {
+		player.body.velocity.rotate(0, 0, rotSpeed);
+	}
 }
