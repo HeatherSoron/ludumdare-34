34dea0f6a5d8b930c520feb29dcc77c80615f42c
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2015-12-12 20:01:57 +1300

    Do some clean-up in the main JS file

--- js/main.js
+++ js/main.js
@@ -5,8 +5,6 @@ var heavy;
 
 var lines = [];
 
-var spaceMode = false;
-
 function makeTerrain() {
 	var iterations = 10;
 	var seaLevel = game.height - (game.height / 4);
@@ -26,8 +24,6 @@ function makeTerrain() {
 			segments.splice(j + 1, 0, mid);
 		}
 	}
-
-	console.log(segments);
 
 
 	var width = game.width / (segments.length - 1);
@@ -52,12 +48,8 @@ function create() {
 	heavy = game.add.sprite(200, 200, 'diamond');
 
 	game.physics.arcade.enable(player);
-	if (spaceMode) {
-		player.body.velocity.x = 60;
-	} else {
-		player.body.gravity.y = 600;
-		player.body.collideWorldBounds = true;
-	}
+	player.body.gravity.y = 600;
+	player.body.collideWorldBounds = true;
 
 	cursors = game.input.keyboard.createCursorKeys();
 
@@ -65,33 +57,13 @@ function create() {
 }
 
 function update() {
-	if (spaceMode) {
-		var rotSpeed = 0.05;
-		var gravPower = 3;
-
-		if (cursors.left.isDown) {
-			player.body.velocity.rotate(0, 0, -rotSpeed);
-		} else if (cursors.right.isDown) {
-			player.body.velocity.rotate(0, 0, rotSpeed);
-		}
-
+	var lineStrength = 0.1;
+	var airResistance = 0.01;
+	if (game.input.activePointer.isDown) {
 		var offset = heavy.position.clone().subtract(player.body.position.x, player.body.position.y);
-		var gravDir = offset.normalize();
-
-		var distSq = offset.getMagnitudeSq();
-
-		var grav = gravDir.setMagnitude(gravPower / distSq);
-
-		player.body.velocity = player.body.velocity.add(grav.x, grav.y);
-	} else {
-		var lineStrength = 0.1;
-		var airResistance = 0.01;
-		if (game.input.activePointer.isDown) {
-			var offset = heavy.position.clone().subtract(player.body.position.x, player.body.position.y);
-			offset.multiply(lineStrength, lineStrength);
-			player.body.velocity.add(offset.x, offset.y);
-			player.body.velocity.multiply(1 - airResistance, 1 - airResistance);
-		}
+		offset.multiply(lineStrength, lineStrength);
+		player.body.velocity.add(offset.x, offset.y);
+		player.body.velocity.multiply(1 - airResistance, 1 - airResistance);
 	}
 }
 
